f1002899431228066329407ccf6b2581e8831129
Артем <artikus.sol@gmail.com>
2023-03-16 19:38:50 +0400

    VER 1.2.1: version & changelog

--- art-show-id.php
+++ art-show-id.php
@@ -5,7 +5,7 @@
  * Text Domain: art-show-id
  * Domain Path: /languages
  * Description: Вывод ID в отдельно колонке для постов, страниц, таксономий, пользователей, комментариев
- * Version: 1.2.0
+ * Version: 1.2.1
  * Author: Artem Abramovich
  * Author URI: https://wpruse.ru/
  * License: GPL-2.0+
@@ -17,7 +17,7 @@
 
 const ASID_PLUGIN_DIR       = __DIR__;
 const ASID_PLUGIN_AFILE        = __FILE__;
-const ASID_PLUGIN_VER       = '1.2.0';
+const ASID_PLUGIN_VER       = '1.2.1';
 const ASID_PLUGIN_SLUG      = 'art-show-id';
 const ASID_PLUGIN_TEPMLATES = 'templates';
 
